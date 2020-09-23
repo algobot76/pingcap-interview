3c1787ad2ff7ebfcbcb400e820b044f23522d2e4
Kaitian Xie <xkaitian@gmail.com>
2020-09-23 03:53:18 -0700

    Add comments in utils

--- packages/utils.py
+++ packages/utils.py
@@ -1,5 +1,15 @@
 import random
 import string
 
+
 def get_random_string(k: int = 1) -> str:
+    """Returns a random string of length k.
+
+    Args:
+        k: Length of a string (default: 1).
+
+    Returns:
+        A random string.
+    """
+
     return ''.join([random.choice(string.ascii_letters) for _ in range(k)])
