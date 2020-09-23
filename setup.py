fdce5fc2357858af0d04618b8437b4533f9bcc76
Kaitian Xie <xkaitian@gmail.com>
2020-09-23 03:56:44 -0700

    Add python_requires

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 setup(
     name="pingcap-interview",
     version="0.0.1",
+    python_requires=">=3.8",
     entry_points={
         'console_scripts': [
             'fuzz = packages.cli:fuzz',
